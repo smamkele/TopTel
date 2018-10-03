441e857bdc650fe02a259008f05fc8212420444f
Smamkele <smsmamkie@gmail.com>
2018-10-03 14:24:53 +0200

    Day7 whereismytransport agencies and lines continue

--- Day7/Part1/script.js
+++ Day7/Part1/script.js
@@ -10,6 +10,28 @@ window.onload = function () {
         var clientSecret = getClientSecret()
          login(clientId,clientSecret)
     })
+  var logoutButton = document.getElementById('logout-form')
+       logoutButton.addEventListener('click', function(event){
+           event.preventDefault()
+           localStorage.removeItem('token')
+           localStorage.removeItem('storageDate')
+       })
+    
+    var submitAgency = document.getElementById('submit-agency')    
+    submitAgency.addEventListener('click', function(event) {
+        event.preventDefault()
+        var agenciesSelectDropdown = document.getElementById('agencies-select')
+        var selectedAgency = agenciesSelectDropdown.options
+        [agenciesSelectDropdown.selectedIndex].value            
+        getLines(getToken(),selectedAgency)
+    })
+}
+function getToken(){
+    var token = this.localStorage.getItem('token')
+    if(token == null || token == undefined || token == 'undefined'){
+      throw new Error('Token invalid')
+    }
+    return token
 }
 function hasToken(){
     var token = localStorage.getItem('token')
@@ -44,11 +66,13 @@ function login(clientId,clientSecret){
             'scope': 'transportapi:all'
 };
 var request = new XMLHttpRequest();
-request.open('POST', 'https://identity.whereismytransport.com/connect/token', true);
-request.addEventListener('load', function () {
+    request.open('POST', 'https://identity.whereismytransport.com/connect/token', true);
+    request.addEventListener('load', function () {
 var response = JSON.parse(this.responseText);
 var token = response.access_token;
 window.token = token;
+
+
 localStorage.setItem('token',token)
 localStorage.setItem('storageDate',Date.now().toLocaleString())
 //   var resultWindow = document.getElementById('result')
@@ -67,7 +91,7 @@ function getAgencies(token){
     var request = new XMLHttpRequest();
     request.addEventListener('load', function () {
         var response = JSON.parse(this.responseText);
-        var agenciesList = document.getElementById('agencies-list')
+        var agenciesList = document.getElementById('agencies-select')
         agenciesList.textContent = this.responseText
         addAgenciesToDropDown(response)
   
@@ -80,9 +104,32 @@ function getAgencies(token){
 }
 function addAgenciesToDropDown(agenciesList) {
     var agenciesSelect = document.getElementById('agencies-select')
-    agenciesSelect.options.length = 0
-    agenciesSelect.options.add(new Option("Select an option", null, true, true))
-    agenciesList.forEach(function(agency) {
+        agenciesSelect.options.length = 0
+        agenciesSelect.options.add(new Option("Select an option", null, true, true))
+        agenciesList.forEach(function(agency) {
         agenciesSelect.options.add(new Option(agency.name, agency.id, false, false))
     })
+}
+function getLines(token,agency){
+    var request = new XMLHttpRequest();
+    request.addEventListener('load', function () {
+        var response = JSON.parse(this.responseText);
+        var linesList = document.getElementById('lines-select')
+            linesList.textContent = this.responseText
+            addLinesToDropDown(response)
+  
+    });
+
+    request.open('GET', 'https://platform.whereismytransport.com/api/lines?agencies=' + agency, true);
+    request.setRequestHeader('Accept', 'application/json');
+    request.setRequestHeader('Authorization', 'Bearer ' + token);
+    request.send();
+}
+function addLinesToDropDown(lineslist){
+    var linesSelect = document.getElementById('lines-select')
+        linesSelect.options.length = 0
+        linesSelect.options.add(new Option("Select an option", null, true, true))
+        lineslist.forEach(function(lines) {
+        linesSelect.options.add(new Option(lines.name, lines.id, false, false))
+})  
 }
